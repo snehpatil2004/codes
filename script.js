815765050c14c3d2997b3994a5beddb8a4d1df1e
snehpatil2004 <snehalpatil.ec22@rvce.edu.in>
2026-01-20 14:36:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Site loaded");
+const codes = {
+
+exp7_ofdm: `
+PASTE CODE FROM:
+ofdm_waveform with diff modulation_exp7
+`,
+
+exp8_cu_du: `
+PASTE CODE FROM:
+cu_du_split_5g_exp8
+`,
+
+exp9_numerology: `
+PASTE CODE FROM:
+exp9_diff5g_numerology
+`,
+
+exp10_csi_rs: `
+PASTE CODE FROM:
+exp10_csi_rs
+`,
+
+exp11_prach: `
+PASTE CODE FROM:
+exp11_prach
+`,
+
+exp12_mimo: `
+PASTE CODE FROM:
+exp12_mimo
+`,
+
+evolution_modulation: `
+PASTE CODE FROM:
+evolution of modulation schemes_6th
+`
+
+};
+
+function copyCode(key) {
+  navigator.clipboard.writeText(codes[key]);
+  alert("Experiment code copied!");
+}
